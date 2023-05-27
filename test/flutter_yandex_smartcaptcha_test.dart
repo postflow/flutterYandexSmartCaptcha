32e8606873698c65792ae5776206ae9dec4b2565
S.Afanasov <sbrin@gmail.com>
2023-05-27 23:48:19 +0300

    .

--- test/flutter_yandex_smartcaptcha_test.dart
+++ test/flutter_yandex_smartcaptcha_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_yandex_smartcaptcha/flutter_yandex_smartcaptcha.dart';
 
 void main() {
   test('adds one to input values', () {
